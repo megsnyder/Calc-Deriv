2f15c3dae83f5b5c6235df4bc87079e9ce5e7c6a
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-11-06 09:36:45 -0500

    Updated from Brython Server: 11/6/2018 9:36:45 AM

--- Program.py
+++ Program.py
@@ -1,1 +1,73 @@
-
+def derivative(equation):
+    if equation.find("=") != -1:
+        equation = equation[equation.find("=")+1:len(equation)]
+    print(getOperandsAndTerms(equation))
+    
+    
+    
+def getOperandsAndTerms(equation):
+    #initial Seperation
+    terms = []
+    term = ""
+    operands = []
+    p = 0
+    op = 1
+    for i in str(equation):
+        if i != " " and i != "'"  and i != "[" and i != "]":
+            if i == "(" or i == "{":
+                p += 1
+                if term != "" and term.count("(") == 0:
+                    terms.append(term)
+                    term = ""
+                    op = 0
+                if op == 0 and p == 1:
+                    #print("OP == 0", term, terms, operands)
+                    operands.append("*")
+            elif i == ")" or i == "}":
+                p -= 1
+            if p == 0 and i != ")" and i != "}":
+                if i == "+" or i == "*" or i == "/" or i == "^":
+                    operands.append(i)
+                    op = 1
+                    if term != "":
+                        terms.append(term)
+                        term = ""
+                elif i == "-":
+                    if op == 1:
+                        op = 2
+                        term += i
+                    elif op == 2:
+                        op = 0
+                        term = term[0:len(term)-1]
+                    else:
+                        #minusOperator
+                        operands.append(i)
+                        op = 1
+                        if term != "":
+                            terms.append(term)
+                            term = ""
+                elif i.isdigit() == True or i == ".":
+                    term += i
+                    op = 0
+                elif i.isdigit() == False:
+                    if term != "":
+                        terms.append(term)
+                        term = ""
+                        op = 0
+                    terms.append(i)
+                if len(terms) > len(operands) + 1:
+                    operands.append("*")
+                    op = 1
+            elif p == 0 and (i == ")" or i == "}"):
+                term += i
+                terms.append(term)
+                term = ""
+                op = 0
+            else:
+                term += i
+    if term != "":
+        terms.append(term)
+    #print("GottenTerms", terms, "GottenOperands", operands, "from", equation)
+    return((terms,operands))
+    
+derivative("y=(69/x-3)^3+x")
